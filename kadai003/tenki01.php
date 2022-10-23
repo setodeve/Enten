593b068951acecc918b1256d4ab1855ba42d4c57
setodeve <keinick11@outlook.com>
2022-10-24 06:14:34 +0900

    Kadai_4

--- kadai003/tenki01.php
+++ kadai003/tenki01.php
@@ -4,23 +4,24 @@
 <title>Title</title>
 </head>
 <body>
-  <form>
-    <fieldset>
+  <form action="tenki02.php">
       <label for="name">名前:</label>
       <input type="text" id="name" name="name"><br><br>
       <label for="post">投稿内容:</label>
       <input type="text" id="post" name="post"><br><br>
+      <fieldset>
         性別:
         <label for="male">男:</label>
-        <input type="radio" id="male" name="gender">
+        <input type="radio" id="male" name="gender" value="male">
         <label for="female">女:</label>
-        <input type="radio" id="female" name="gender">
+        <input type="radio" id="female" name="gender" value="female">
         <label for="NoAnswer">無回答:</label>
-        <input type="radio" id="NoAnswer" name="gender"><br><br>
-        <label for="mark">目立つマーク:</label>
+        <input type="radio" id="NoAnswer" name="gender" value="NoAnswer"><br><br>
+      </fieldset>
+      <label for="mark">目立つマーク:</label>
       <input type="checkbox" id="mark" name="mark"><br><br>
       <input type="submit" value="送信">
-    </fieldset>
+
   </form>
 
 </body>
---
+++ kadai003/tenki02.php
@@ -0,0 +1,6 @@
+<?php
+  error_log($_GET["name"], 0);
+  error_log($_GET["post"], 0);
+  error_log($_GET["gender"], 0);
+  error_log($_GET["mark"], 0);
+?>
