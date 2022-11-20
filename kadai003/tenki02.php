aef11c1fbb659ffa1ec4d46f56584de10ef8e2e1
setodeve <keinick11@outlook.com>
2022-11-20 21:10:44 +0900

    kadai_7

--- kadai003/tenki02.php
+++ kadai003/tenki02.php
@@ -1,3 +1,13 @@
+
+<p>
+  <?php echo $_POST["name"] ;?>
+</p>
+<form action="tenki03.php" method="post">
+  <div>
+      <input type="hidden" id="name" name="name" value="<?php echo $_POST["name"] ;?>">
+      <input type="submit" value="確認">
+  </div>
+</form>
 <?php
 
   function getRequest(){
@@ -8,7 +18,6 @@
       error_log($_GET["mark"], 0);
       error_log("Got", 0);
     }elseif(!empty($_POST)){
-      echo array_key_exists("name",$_POST);
       error_log($_POST["name"], 0);
       error_log($_POST["post"], 0);
       error_log($_POST["gender"], 0);
--- kadai003/tenki03.php
+++ kadai003/tenki03.php
@@ -1,3 +1,4 @@
 <?php
-
+  echo  $_POST["name"];
+  error_log($_POST["name"], 0);
 ?>
