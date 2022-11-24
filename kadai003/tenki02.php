2b05cea9271543c87abd62d18147747aed589410
setodeve <keinick11@outlook.com>
2022-11-24 22:43:35 +0900

    kadai_10

--- kadai003/tenki02.php
+++ kadai003/tenki02.php
@@ -5,6 +5,9 @@
 <form action="tenki03.php" method="post">
   <div>
       <input type="hidden" id="name" name="name" value="<?php echo $_POST["name"] ;?>">
+      <input type="hidden" id="post" name="post" value="<?php echo $_POST["post"] ;?>">
+      <input type="hidden" id="gender" name="gender" value="<?php echo $_POST["gender"] ;?>">
+      <input type="hidden" id="mark" name="mark" value="<?php echo $_POST["mark"] ;?>">
       <input type="submit" value="確認">
   </div>
 </form>
--- kadai003/tenki03.php
+++ kadai003/tenki03.php
@@ -1,5 +1,12 @@
 <?php
+  $data = [
+    'name' => $_POST["name"],
+    'post' => $_POST["post"],
+    'gender' => $_POST["gender"],
+    'mark' => $_POST["mark"]    
+  ];
   echo  $_POST["name"];
   error_log($_POST["name"], 0);
   $pdo = new PDO( 'mysql:host=localhost;dbname=kadaidb;charset=utf8', 'root', '' );
-?>
+  $stmt = $pdo->prepare ( 'insert into tenki (name,post,gender,mark) values(:name, :post,:gender,:mark)' );
+  $stmt->execute($data);
