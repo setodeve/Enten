15a7ad791a41fbe5fba40ef822a7c7ad879b9324
setodeve <keinick11@outlook.com>
2022-11-25 22:33:04 +0900

    edit tenki04

--- kadai003/tenki04.php
+++ kadai003/tenki04.php
@@ -1,6 +1,13 @@
 <?php
-$stmt = $pdo->prepare ( 'select * from tenki ' );
+$pdo = new PDO( 'mysql:host=localhost;dbname=kadaidb;charset=utf8', 'root', '' );
+$stmt = $pdo->prepare( 'select * from tenki ' );
 $stmt->execute();
 $list = $stmt->fetchAll();
-  echo $list;
+
+for($i=0;$i<count($list);$i++){
+  // echo $list[$i]["name"];
+  echo '<br>';
+  echo '<a href="tenki05.php">'. $list[$i]["name"].'</a>';
+}
 ?>
+
